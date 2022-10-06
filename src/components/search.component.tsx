da25b03c407443125294de0420de923444f72815
Matej Kukolik <dalinec123@gmail.com>
2022-10-06 15:58:34 +0200

    add search component

---
+++ src/components/search.component.tsx
@@ -0,0 +1,61 @@
+import { FC, FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setAlert } from '../store/actions/alert.action';
+import { getWeather, setLoading } from '../store/actions/weather.actions';
+
+type SearchProps = {
+  title: string;
+};
+
+const Search: FC<SearchProps> = ({ title }) => {
+  const [city, setCity] = useState('');
+  const dispatch = useDispatch();
+
+  const changeHandler = (e: FormEvent<HTMLInputElement>): void => {
+    setCity(e.currentTarget.value);
+  };
+
+  const submitHandler = (
+    e: FormEvent | React.SyntheticEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+
+    if (city.trim() === '') {
+      return dispatch(setAlert('City is required'));
+    }
+
+    dispatch(setLoading());
+    dispatch(getWeather(city));
+    setCity('');
+    return;
+  };
+
+  return (
+    <div className='hero is-light has-text-centered'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title'>{title}</h1>
+          <form className='py-5' onSubmit={submitHandler}>
+            <input
+              type='text'
+              className='input has-text-centered mb-2'
+              placeholder='Enter a City'
+              style={{ maxWidth: 300 }}
+              value={city}
+              onChange={changeHandler}
+            />
+            <button
+              className='button is-primary is-fullwidth'
+              style={{ maxWidth: 300, margin: '0 auto' }}
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
